a1474069080fabe68bb0a9d82fdd8160812a7c99
Oktober13 <lydiazuehsow@gmail.com>
2016-03-09 14:52:44 -0500

    Added files via upload

--- object_movement4.py
+++ object_movement4.py
@@ -43,24 +43,22 @@ class WebCam(object):
 		#find contours in the mask, initialize current (x,y) center
 		self.cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,
 			cv2.CHAIN_APPROX_SIMPLE)[-2]
+		center = None
 
-		#only continue if at least one contour is found
+		#only proceed if at least one contour was found
 		if len(self.cnts) > 0:
 
 			#find largest contour in mask, use it to compute 
-			#minimum enclosing circel and centroid for that contour
-
+			#minimum enclosing circel and centroid
 			c = max(self.cnts,key=cv2.contourArea)
-			M = cv2.moments(c)
-			((self.x,self.y),self.radius) = cv2.minEnclosingCircle(c)
-			Mlist= [M["m10"], M["m00"],M["m01"],M["m00"]]
-			if any(Mlist) == 0:
-				pass
-				return False
-			else:
-				center = (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"]))
 
-				return center
+			# finds approximate shape of object
+			epsilon = 0.01*cv2.arcLength(c,True)
+			self.approx = cv2.approxPolyDP(c,epsilon,True)
+			((self.x,self.y),self.radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"]))
+			return center
 
 class PygameView(object):
 	"""Visualizes a fake desktop in a pygame window"""
@@ -96,36 +94,30 @@ class DesktopModel(object):
 
 class OpenCVController(object):
 	def __init__(self,model):
+		self.model = model
 
+	def getEvent(self, model, event):
 		"""Look for left and right Pygame Events"""
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
-		elif event.type == MOUSEMOTION:
-			mousex,mousey = event.pos
-		elif event.type == GREENMOVEH:
+		if event.type == pygame.MOUSEMOTION:
+			model.cursor.x,model.cursor.y = event.pos
+		if event.type == GREENMOVEH:
 			if 0 < (mousex + dX/300) < screenwidth:
-				mousex = mousex - (dX/100)
-			if mousex > screenwidth:
-				mousex = screenwidth - 10
-			if mousex < 0:
-				mousex = 10
-			pygame.mouse.set_pos(mousex,mousey)
-		elif event.type == GREENMOVEV:
-			if 0 < (mousey + dY/600) < screenheight:
-				mousey = mousey - (dY/600)
-			if mousey > screenheight:
-				mousey = screenheight -10
-			if mousey < 0:
-				mousey = 10
-			pygame.mouse.set_pos(mousex,mousey)
-		else:
-			pass
+				model.cursor.x = model.cursor.x - (dX/100)
+			if model.cursor.x > screenwidth:
+				model.cursor.x = screenwidth - 10
+			if model.cursor.x < 0:
+				model.cursor.x = 10
+			pygame.mouse.set_pos(model.cursor.x,model.cursor.y)
+		if event.type == GREENMOVEV:
+			if 0 < (model.cursor.y + dY/600) < screenheight:
+				model.cursor.y = model.cursor.y - (dY/600)
+			if model.cursor.y > screenheight:
+				model.cursor.y = screenheight -10
+			if model.cursor.y < 0:
+				model.cursor.y = 10
+			pygame.mouse.set_pos(model.cursor.x,model.cursor.y)
 
 if __name__ == '__main__':
-
-	"""Initializing"""
-
 	#Initialize pygame
 	pygame.init()
 
@@ -142,16 +134,18 @@ if __name__ == '__main__':
 	size = (screenwidth, screenheight)
 	screen = pygame.display.set_mode(size)
 
-	model = DesktopModel()
-	view = PygameView(model, screen)
-
+	# catSurfaceObj= pygame.image.load('mouse.png')
 	# mousex,mousey = (screenwidth/2,screenheight/2)
 
-	# #Create new event for vertical and horizontal green movements
-	# GREENMOVEH = pygame.USEREVENT+1
-	# moveH_event= pygame.event.Event(GREENMOVEH)
-	# GREENMOVEV = pygame.USEREVENT+2
-	# moveV_event= pygame.event.Event(GREENMOVEV)
+	model = DesktopModel()
+	view = PygameView(model, screen)
+	controller = OpenCVController(model)
+
+	#Create new event for vertical and horizontal green movements
+	GREENMOVEH = pygame.USEREVENT+1
+	moveH_event= pygame.event.Event(GREENMOVEH)
+	GREENMOVEV = pygame.USEREVENT+2
+	moveV_event= pygame.event.Event(GREENMOVEV)
 
 	"""WEBCAM STUFF"""
 
@@ -159,6 +153,7 @@ if __name__ == '__main__':
 	greenUpper= (64,255,255)
 
 	(dX,dY) = (0,0)
+	direction = ""
 
 	webcam = WebCam()
 
@@ -170,28 +165,8 @@ if __name__ == '__main__':
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				running = False
-				pygame.quit()
-				sys.exit()
-			# if event.type == MOUSEMOTION:
-# # 			mousex,mousey = event.pos
-# # 		if event.type == GREENMOVEH:
-# # 			if 0 < (mousex + dX/300) < screenwidth:
-# # 				mousex = mousex - (dX/100)
-# # 			if mousex > screenwidth:
-# # 				mousex = screenwidth - 10
-# # 			if mousex < 0:
-# # 				mousex = 10
-# # 			pygame.mouse.set_pos(mousex,mousey)
-# # 		if event.type == GREENMOVEV:
-# # 			if 0 < (mousey + dY/600) < screenheight:
-# # 				mousey = mousey - (dY/600)
-# # 			if mousey > screenheight:
-# # 				mousey = screenheight -10
-# # 			if mousey < 0:
-# # 				mousey = 10
-# # 			pygame.mouse.set_pos(mousex,mousey)
-# 		else:
-# 			continue
+			#Get cursor position changes from controller.
+			controller.getEvent(model, event)
 
 		#Find the center of any green objects' contours
 		center = webcam.getcenter(greenLower, greenUpper)
@@ -201,7 +176,7 @@ if __name__ == '__main__':
 			if webcam.radius > 10:
 				#draw contour and centroid on frame,
 				#update list of tracked points
-				# cv2.drawContours(webcam.frame,webcam.approx,-1, (0,255,0),3)
+				cv2.drawContours(webcam.frame,webcam.approx,-1, (0,255,0),3)
 
 				# Draws the circle with the area equal to the area of the contour
 				# cv2.circle(webcam.frame,(int(webcam.x),int(webcam.y)),int(webcam.radius),
@@ -227,3 +202,7 @@ if __name__ == '__main__':
 
 		time.sleep(.001)
 
+	if running == False:
+		#release camera, close open windows
+		webcam.camera.release()
+		cv2.destroyAllWindows()
